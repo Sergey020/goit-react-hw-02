cb83ee27da755a5952802fef6b135d0309f61887
Sergey020 <114489697+Sergey020@users.noreply.github.com>
2024-05-25 17:02:25 +0300

    shag3

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,33 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Description from "./components/Description/Description"
+import Feedback from './components/Feedback/Feedback'
+import Options from './components/Options/Options'
+import Notification from './components/Notification/Notification'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 })
+  const totalFeedback = feedback.good + feedback.bad + feedback.neutral;
+  const updateFeedback = feedbackType => {
+    setFeedback({
+      ...feedback,
+      [feedbackType]: feedback[feedbackType] + 1
+    })
+  };
+  // const resetFeedback = () => {
+  //   setFeedback({ good: 0, neutral: 0, bad: 0 });
+  // };
 
   return (
-    <>
-      
-    </>
+      <div>
+      <h1>Sip Happens Café</h1>
+      <p>Please leave your feedback about our service by selecting one of the options below.</p>
+      <Options updateFeedback={updateFeedback}/>
+      { totalFeedback > 0 ? <Feedback feedback={feedback}/> : <Notification/> }
+      {/* <Options onFeedback={handleFeedback} onReset={resetFeedback} />
+      <Feedback feedback={feedback} total={totalFeedback} positive={positiveFeedback} /> */}
+    </div>
   )
 }
 
